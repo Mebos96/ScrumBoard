6ddc2796c257a1e28bc36829549c0dfd3626467d
Bartek <bartosz.adamczuk@gmail.com>
2020-02-28 00:16:48 +0100

    Add card Form

--- src/components/List.js
+++ src/components/List.js
@@ -3,58 +3,6 @@ import Card from './Card'
 import styled from 'styled-components'
 import { Droppable } from 'react-beautiful-dnd'
 
-const ListContainer = styled.div`
-    display:flex;
-    flex-direction:column;
-    align-items:center;
-    background:#999;
-    border-radius:5px;
-    width:250px;
-    margin:10px;
-    position: relative;
-    min-height:110px;
-    height:100%;
-    box-shadow: 3px 3px 7px #444;
-`
-const ListTitle = styled.h2`
-    padding:10px 5px;
-    color:#fff;
-    text-align:center
-`
-const CardsContainer = styled.div`
-    width:100%;
-    position: relative;
-    display:flex;
-    flex-wrap:wrap;
-    justify-content:center;
-    margin:5px 0;
-`
-const AddCardButton = styled.button`
-    width:95%;
-    margin:5px;
-    border-radius:5px;
-    background:#4cd137;
-    border:none;
-    cursor: pointer;
-`
-const AddCardButtonText = styled.h3`
-    color:#fff;
-    margin:5px
-`
-const AddCardForm = styled.div`
-    display: ${props => {
-        if(props.visible ===true) return "flex"
-        else return "none"
-    }};
-    flex-direction: column;
-    width:95%;
-`
-const TitleInput = styled.input`
-
-`
-const TextInput = styled.textarea`
-
-`
 export default function List({list}) {
     const [title,setTitle]=useState('')
     const [text,setText]=useState('')
@@ -84,19 +32,26 @@ export default function List({list}) {
                 <React.Fragment>
                     <AddCardForm visible={isVisible}>
                         <TitleInput
+                            placeholder="Title"
                             value={title}
-                            onChange={title=> console.log(title)}
+                            onChange={(e)=>setTitle(e.target.value)}
                         />
                         <TextInput
+                            placeholder="Text"
                             value={text}
-                            onChange={(text)=>console.log(text)}
+                            onChange={(e)=>setText(e.target.value)}
                         />
                     </AddCardForm>
-                    <AddCardButton onClick={addCard}>
-                        <AddCardButtonText>
-                            Add task
-                        </AddCardButtonText>
-                    </AddCardButton>
+                    <Buttons>
+                        <AddCardButton onClick={addCard} visible={isVisible}>
+                            <AddCardButtonText>
+                                Add task
+                            </AddCardButtonText>
+                        </AddCardButton>
+                        <CloseButton onClick={()=>setIsVisible(!isVisible)} visible={isVisible}>
+                            ✕
+                        </CloseButton>
+                    </Buttons>
                 </React.Fragment>
                 :null
             }
@@ -104,3 +59,90 @@ export default function List({list}) {
         </ListContainer>
     )
 }
+const ListContainer = styled.div`
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    background:#999;
+    border-radius:5px;
+    width:250px;
+    margin:10px;
+    position: relative;
+    min-height:110px;
+    height:100%;
+    box-shadow: 3px 3px 7px #444;
+`
+const ListTitle = styled.h2`
+    padding:10px 5px;
+    color:#fff;
+    text-align:center
+`
+const CardsContainer = styled.div`
+    width:100%;
+    position: relative;
+    display:flex;
+    flex-wrap:wrap;
+    justify-content:center;
+    margin:5px 0;
+`
+const Buttons = styled.div`
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    width:100%;
+`
+const AddCardButton = styled.button`
+    width: ${props => {
+        if(props.visible === true) return "85%"
+        else return "95%"
+    }};
+    margin:5px;
+    border-radius:5px;
+    background:#4cd137;
+    border:none;
+    cursor: pointer;
+    outline:none;
+    &:active{
+        background:#3bc026
+    }
+`
+const CloseButton = styled.p`
+    display: ${props => {
+        if(props.visible === true) return "block"
+        else return "none"
+    }};
+    padding:5px;
+    font-weight:bold;
+    cursor: pointer;
+    color:#333
+`
+const AddCardButtonText = styled.h3`
+    color:#fff;
+    margin:5px
+`
+const AddCardForm = styled.div`
+    display: ${props => {
+        if(props.visible ===true) return "flex"
+        else return "none"
+    }};
+    flex-direction: column;
+    width:95%;
+`
+const TitleInput = styled.input`
+    margin:2px;
+    height:20px;
+    outline:none;
+    &::placeholder{
+        padding:0 3px
+    }
+`
+const TextInput = styled.textarea`
+    margin:2px;
+    min-height:50px;
+    resize:none;
+    outline:none;
+    &::placeholder{
+        padding:0 3px
+    }
+    
+`
